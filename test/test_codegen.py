7faf8b4531b79f04258276360835e888492ced63
Vennela610 <alavalavennela@gmail.com>
2026-08-24 22:29:46 +0530

    Actions for UI using codegen

--- test/test_codegen.py
+++ test/test_codegen.py
@@ -36,4 +36,43 @@ def test_run(playwright: Playwright) -> None:
 
 
 with sync_playwright() as playwright:
-    test_run(playwright)
+    test_run(playwright) 
+
+
+
+import re
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+
+def run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://testautomationpractice.blogspot.com/")
+    page.get_by_role("link", name="2", exact=True).click()
+    page.locator("td:nth-child(4) > input").first.check()
+    page.get_by_role("link", name="3", exact=True).click()
+    page.locator("tr:nth-child(3) > td:nth-child(4) > input").check()
+    page.get_by_role("link", name="4", exact=True).click()
+    page.locator("tr:nth-child(5) > td:nth-child(4) > input").check()
+    page.get_by_role("textbox", name="Select an item").click()
+    page.get_by_text("Item 43").click()
+    page.locator(".ui-slider-handle").first.click()
+    page.locator(".ui-slider-range").click()
+    page.locator(".svg-container").click()
+    page.locator("#draggable").click()
+    page.locator("#draggable").click()
+    page.locator("#draggable").click()
+    page.locator("#draggable").click()
+    page.locator("#draggable").click()
+
+    # ---------------------
+    context.close()
+    browser.close()
+
+
+with sync_playwright() as playwright:
+    run(playwright)
+
+
+
